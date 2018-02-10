3445934f694463873592b1deedc47d2a355c5a1e
Raniah Abdul Razak <raniarazzak@gmail.com>
2018-02-10 12:25:44 +0800

    work

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -10,6 +10,22 @@ namespace Day1
     {
         static void Main(string[] args)
         {
+            //writeline means writing the string and then change line
+            Console.WriteLine("Hello");
+
+            //WRITE MEANS WRITING THE STRING TO THE SCREEN and no next line
+            Console.Write("Hello");
+
+            string name;
+
+            Console.WriteLine("Enter a name!");
+            name = Console.ReadLine();
+            // wait for user to key in a value and press enter
+            //and assign the value into a variable called 'name'
+
+
+            Console.WriteLine(name);
+
         }
     }
 }
